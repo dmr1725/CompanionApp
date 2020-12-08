85acc7add841f4c3037ca792f428b5a8ff906061
dmr1725 <diegozmendez@gmail.com>
2020-12-08 13:57:03 -0400

    changes to json files for using one comma

--- organizar/organizar.py
+++ organizar/organizar.py
@@ -3,9 +3,11 @@ import json
 
 path1 = "C:/Users/diego/Documents/companion_app_gh/primer_sem"
 path2 = "C:/Users/diego/Documents/companion_app_gh/segundo_sem"
+path3 = "C:/Users/diego/Documents/companion_app_gh/Miupi Parser"
 
 dirs1 = os.listdir(path1)
 dirs2 = os.listdir(path2)
+dirs3 = os.listdir(path3)
 
 def orderFiles1(files1):
     i = 1
@@ -30,10 +32,22 @@ def orderFiles3(files1, files2, files3):
         files3.append({'file': files2[i]['file'], 'num': files1[i]['num']})
     return files3
 
+def orderFilesProxSemestre(files):
+    """ 
+    Organizing all the files from Miupi Parser to a single array. The array will contain each directory of each file
+    """
+    i = 1
+    for file in dirs3:
+        if file.endswith('.json'):
+            files.append({'file': file, 'num': i})
+            i += 1 # parse.py esta entre medio
+    return files
+
 files1 = orderFiles1([])
 files2 = orderFiles2([])
 files3 = []
 files3 = orderFiles3(files1, files2, files3)
+proxSemFiles = orderFilesProxSemestre([])
 
 
 
